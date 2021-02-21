b4d0334ffcccd853c5bedfb359871f5434f4ebdd
AMMIROSOH <a.sohrabi1380.fake@gmail.com>
2021-02-21 14:41:49 +0330

    added some datasets

--- test.py
+++ test.py
@@ -2,14 +2,14 @@ import csv
 import pandas as pd
 from main import LinearRegression
 
-file = pd.read_csv('student-mat.csv', sep=";")
+file = pd.read_csv('Admission_Predict.csv', sep=",")
 length = len(file)
 absences = []
 failures = []
 
 for i in range(0, length):
-    absences.append(file['absences'][i])
-    failures.append(file['failures'][i])
+    absences.append(file['GRE Score'][i])
+    failures.append(file['Chance of Admit '][i])
 
 
 ln = LinearRegression([absences, failures], sliced=True)
